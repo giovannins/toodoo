a356672a9e0ebde0a5d1b8105815dc25f461ade7
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-11-28 14:56:24 -0300

    Criar em caso não exista

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Access;
+use App\Repository\AccessRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,13 +26,29 @@ class LoginController extends AbstractController
     }
 
     #[Route('/login', name: 'app_login', methods: ['POST'])]
-    public function login(Request $req): Response
+    public function login(Request $req, ManagerRegistry $doc, AccessRepository $access): Response
     {
+        $password = $req->get('password');
+        $db_pass = $access->findOneBy(['password' => $password]);
+
         if (strlen($req->get('password')) < 8) {
             return new RedirectResponse('/?error=small_password');
         }
 
-        return new Response($req->get('password'));
+        if ($db_pass) {
+            return new Response($req->get('password'));
+        }
+
+        $manager = $doc->getManager();
+
+        $new_access = new Access();
+        $new_access->setPassword($password);
+        
+        $manager->persist($new_access);
+        $manager->flush();
+
+        return new Response($new_access->getId());
+
     }
 
     #[Route('/logout', name: 'app_logout', methods: ['GET', 'POST'])]
