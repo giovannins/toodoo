2cbdbc0108805c382dbe2c6a56229c730a885af8
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-12-02 10:11:48 -0300

    Mostrando toodoos

--- src/Controller/TodolistController.php
+++ src/Controller/TodolistController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserlistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -11,16 +12,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class TodolistController extends AbstractController
 {
     #[Route('/todolist', name: 'todolist_index', methods: ['GET'])]
-    public function index(RequestStack $requestStack): Response
+    public function index(RequestStack $requestStack, UserlistRepository $userlistRepository): Response
     {
         $session = $requestStack->getSession();
-
+        $todo = $userlistRepository->findBy(['access_id' => $session->get('id')]);
+        // dd($todo);
         if ($session->get('login')) {
             return $this->render('todolist/index.html.twig', [
                 'app_name' => $_ENV['APP_NAME'],
+                'todo' => $todo,
+                'id' => $session->get('id')
             ]);
         }
 
         return new RedirectResponse('/');
     }
+
+    
 }
